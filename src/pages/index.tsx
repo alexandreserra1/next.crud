cd071c18053adf14454cf73b8c1be0f94a7376ab
aserra <alexandreapserra@hotmail.com>
2023-11-06 22:05:38 -0300

    Primeiro commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home(){
+  return
+  <div className={`
+  flex justify-center items-center h-screen
+  `}>
+    <span className="text-4xl">APP</span>
+  </div>
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'tailwindcss'
+
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  darkMode: 'class', // ou 'media' ou 'class' ou 'false Configuração do modo escuro para alternar usando classes
+  plugins: [],
+}
+ 
